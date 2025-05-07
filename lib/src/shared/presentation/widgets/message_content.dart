87a4a3b4f9062d2d0720aced83cdf78675f00212
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-05-07 14:52:08 -0400

    Impl Chat Features

--- lib/src/shared/presentation/widgets/message_content.dart
+++ lib/src/shared/presentation/widgets/message_content.dart
@@ -10,24 +10,38 @@ class MessageContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    final ScrollController scrollController = ScrollController();
+
     return Container(
       padding: EdgeInsets.all(16.0),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildSystemMessage(title ?? 'Título del mensaje'),
+          _buildSystemMessage(title),
+          Expanded(
+              child: Scrollbar(
+                thumbVisibility: true,
+                interactive: true,
+                controller: scrollController,
+                child: ListView.separated(
+                  controller: scrollController,
+                  shrinkWrap: true,
+                  physics: BouncingScrollPhysics(),
+                  padding: EdgeInsets.only(right: 16.0),
+                  itemCount: content.length,
+                  separatorBuilder: (context, index) => const SizedBox(height: 10),
+                  itemBuilder: (context, index) {
+                    return _buildInfoCard(
+                      title: content[index].member,
+                      content: content[index].text,
+                    );
+                  },
+                ),
+             )
+          )
 
-          ListView.separated(
-            shrinkWrap: true,
-            physics: const AlwaysScrollableScrollPhysics(),
-            itemCount: content.length,
-            separatorBuilder: (context, index) => const SizedBox(height: 10),
-            itemBuilder: (context, index) {
-              return _buildInfoCard(
-                title: content[index].member,
-                content: content[index].text,
-              );
-            },
-          ),
         ],
       ),
     );
@@ -46,21 +60,35 @@ class MessageContent extends StatelessWidget {
   }
 
   Widget _buildInfoCard({required String title, required String content}) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              title,
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        if (title == 'You') SizedBox(width: 45),
+        Expanded(
+          child: Card(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment:
+                    title == 'You'
+                        ? CrossAxisAlignment.end
+                        : CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    title,
+                    textAlign: title == 'You' ? TextAlign.end : TextAlign.start,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+                  ),
+                  SizedBox(height: 8),
+                  Text(content, textAlign: TextAlign.justify, style: TextStyle(),),
+                ],
+              ),
             ),
-            SizedBox(height: 8),
-            Text(content),
-          ],
+          ),
         ),
-      ),
+        if (title != 'You') SizedBox(width: 45),
+        // Espacio entre la tarjeta y el borde
+      ],
     );
   }
 }
