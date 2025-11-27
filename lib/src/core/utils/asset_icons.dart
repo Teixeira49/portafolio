690d917e18a683ea65ac6faa7b8dfb0d7ceeda27
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-11-27 19:02:58 -0400

    Create assets util files
    - Add AssetImages class for image paths
    - Add AssetIcons class for icon paths

---
+++ lib/src/core/utils/asset_icons.dart
@@ -0,0 +1,87 @@
+class AssetIcons {
+  static const String _iconRoute = 'icons';
+
+  static const String iconCanva = '$_iconRoute/canva.svg';
+
+  static const String iconCss = '$_iconRoute/css_old.svg';
+
+  static const String iconDJango = '$_iconRoute/django.svg';
+
+  static const String iconFastApi = '$_iconRoute/fastapi.svg';
+
+  static const String iconFigma = '$_iconRoute/figma.svg';
+
+  static const String iconFirebase = '$_iconRoute/firebase.svg';
+
+  static const String iconFirebaseStudio = '$_iconRoute/firebase_studio.svg';
+
+  static const String iconFlet = '$_iconRoute/flet.svg';
+
+  static const String iconFlutter = '$_iconRoute/flutter.svg';
+
+  static const String iconGithubDark = '$_iconRoute/github_dark.svg';
+
+  static const String iconGithubLight = '$_iconRoute/github_light.svg';
+
+  static const String iconGmail = '$_iconRoute/gmail.svg';
+
+  static const String iconGoogle = '$_iconRoute/google.svg';
+
+  static const String iconHtml = '$_iconRoute/html5.svg';
+
+  static const String iconJava = '$_iconRoute/java.svg';
+
+  static const String iconJavascript = '$_iconRoute/javascript.svg';
+
+  static const String iconJira = '$_iconRoute/jira.svg';
+
+  static const String iconKotlin = '$_iconRoute/kotlin.svg';
+
+  //static const String iconLinux = '$_iconRoute/linux.svg';
+
+  //static const String iconMongodb = '$_iconRoute/mongodb.svg';
+
+  static const String iconLinkedin = '$_iconRoute/linkedin.svg';
+
+  //static const String iconMysql = '$_iconRoute/mysql.svg';
+
+  static const String iconMicrosoft365 = '$_iconRoute/microsoft_365.svg';
+
+  static const String iconNestJs = '$_iconRoute/nestjs.svg';
+
+  static const String iconNextJs = '$_iconRoute/nextjs_icon_dark.svg';
+
+  static const String iconNodeJs = '$_iconRoute/nodejs.svg';
+
+  static const String iconOffice = '$_iconRoute/office.svg';
+
+  static const String iconPhotoshop = '$_iconRoute/photoshop.svg';
+
+  static const String iconPostgreSQL = '$_iconRoute/postgresql.svg';
+
+  static const String iconPostman = '$_iconRoute/postman.svg';
+
+  static const String iconPython = '$_iconRoute/python.svg';
+
+  static const String iconPowershell = '$_iconRoute/powershell.svg';
+
+  static const String iconReactDark = '$_iconRoute/react_dark.svg';
+
+  static const String iconReactLight = '$_iconRoute/react_light.svg';
+
+  static const String iconScrum = '$_iconRoute/scrum.svg';
+
+  static const String iconSlack = '$_iconRoute/slack.svg';
+
+  static const String iconSpring = '$_iconRoute/spring.svg';
+
+  static const String iconTailwind = '$_iconRoute/tailwind_css.svg';
+
+  static const String iconTrello = '$_iconRoute/trello.svg';
+
+  static const String iconWhatsApp = '$_iconRoute/whatsapp_icon.svg';
+
+  static const String profile = '$_iconRoute/profile_icon.svg';
+
+  static const String iconLock = '$_iconRoute/lock.svg';
+}
---
+++ lib/src/core/utils/asset_images.dart
@@ -0,0 +1,13 @@
+class AssetImages {
+  static const String _images = 'images';
+
+  static const String ceisLogo = '$_images/ceis_unimet_logo.png';
+
+  static const String emptyImage = '$_images/empty_image.png';
+
+  static const String profile = '$_images/profile.jpg';
+
+  static const String unimetBanner = '$_images/unimet.png';
+
+  static const String unimetLogo = '$_images/unimet_logo.png';
+}
