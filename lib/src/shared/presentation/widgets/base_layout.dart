3b4d123d90b5e847ccc97634c20d764a1ede4698
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-11-28 15:42:43 -0400

    fix responsive button for deploy drawer

--- lib/src/shared/presentation/widgets/base_layout.dart
+++ lib/src/shared/presentation/widgets/base_layout.dart
@@ -1,4 +1,3 @@
-
 // main_layout_page.dart
 
 import 'package:flutter/material.dart';
@@ -6,16 +5,17 @@ import 'package:flutter/material.dart';
 import 'navigator_panel.dart';
 
 class BaseLayoutPage extends StatefulWidget {
-  final Widget child; // El contenido principal que se mostrará a la derecha
+  final Widget child;
+  final Widget? title;
 
-  const BaseLayoutPage({super.key, required this.child});
+  const BaseLayoutPage({super.key, required this.child, this.title});
 
   @override
   State<BaseLayoutPage> createState() => _BaseLayoutPageState();
 }
 
 class _BaseLayoutPageState extends State<BaseLayoutPage> {
-  bool _isPanelOpen = true; // Por defecto, el panel está abierto
+  bool _isPanelOpen = true;
 
   void _togglePanel() {
     setState(() {
@@ -25,38 +25,29 @@ class _BaseLayoutPageState extends State<BaseLayoutPage> {
 
   @override
   Widget build(BuildContext context) {
-    // Usamos LayoutBuilder para obtener las dimensiones de la pantalla y hacerlo adaptable
     return LayoutBuilder(
       builder: (context, constraints) {
-        // Define los tamaños del panel
-        final double maxPanelWidth = 300.0; // Ancho máximo cuando está abierto
-        final double minPanelWidth = 80.0;  // Ancho mínimo cuando está colapsado
-        final bool isMobile = constraints.maxWidth < 600; // Define un punto de quiebre para móviles
+        final double maxPanelWidth = 300.0;
+        final double minPanelWidth = 80.0;
+        final bool isMobile = constraints.maxWidth < 600;
 
         final panelWidth = constraints.maxWidth * 0.25;
 
-        // Si es móvil, usamos un Drawer tradicional. Si no, usamos el panel persistente.
         if (isMobile) {
           return Scaffold(
-            drawer: NavigationPanel(
-              width: maxPanelWidth, // El drawer siempre tiene su ancho máximo
-            ),
+            appBar: AppBar(title: widget.title),
+            drawer: NavigationPanel(width: maxPanelWidth),
             body: widget.child,
           );
         } else {
-          // Layout para pantallas grandes (tablets, web, desktop)
           return Scaffold(
             body: Row(
               children: [
-                // Panel de Navegación Animado
                 NavigationPanel(
-                    width: _isPanelOpen ? panelWidth : minPanelWidth,
-                  ),
-
-                // Contenido Principal
-                Expanded(
-                  child: widget.child, // Aquí va el contenido de la página (ej. tu chat)
+                  width: _isPanelOpen ? panelWidth : minPanelWidth,
                 ),
+
+                Expanded(child: widget.child),
               ],
             ),
           );
