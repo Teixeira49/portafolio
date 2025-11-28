631a7b53021182b9b3ab82bbbadae1843bdc7a8a
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-11-27 23:00:31 -0400

    test without env

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 import 'package:portafolio/src/core/providers/providers.dart';
 import 'package:portafolio/src/core/theme/app_theme.dart';
 import 'package:portafolio/src/core/variables/constants/constants.dart';
@@ -10,7 +10,7 @@ import 'package:provider/provider.dart';
 import 'l10n/app_localizations.dart';
 
 void main() async {
-  await dotenv.load();
+ //await dotenv.load();
   setupLocator();
   runApp(const MyApp());
 }
