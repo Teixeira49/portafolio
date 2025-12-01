257269c4566e4962f789cb512e5df55e8ca49578
Teixeira49 <javierarturoteixeiragoncalves@gmail.com>
2025-12-01 15:58:41 -0400

    Update about data

--- lib/src/shared/data/datasource/local/about.dart
+++ lib/src/shared/data/datasource/local/about.dart
@@ -1,9 +1,11 @@
+import 'package:portafolio/src/shared/data/datasource/local/projects.dart';
+
 import '../../../../core/variables/constants/constants.dart';
 
 Map<String, dynamic> aboutData = {
   "name": 'Javier Teixeira',
   "location": Constants.location,
   "location_link": Constants.locationLink,
-  "years": 3,
-  "projects": 10,
+  "years": 2023,
+  "projects": projects.length,
 };
